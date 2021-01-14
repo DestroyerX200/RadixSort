142e07cf598163cea6bd7ca1c2cb50f11b63d977
i has candys <ichen10@stuy.edu>
2021-01-13 20:37:02 -0500

    modified nth method in Radix class

--- Radix.java
+++ Radix.java
@@ -1,7 +1,10 @@
 public class Radix {
 	public static int nth(int n, int col) {
-		n %= Math.pow(10, col+1); //truncating digits to the left of col
-		n = (n - (n % ((int) Math.pow(10, col)) ) ) / (int) Math.pow(10, col);
-		return Math.abs(n);
+		int returnNum = Math.abs(n);
+		returnNum %= Math.pow(10, col+1); //truncating digits to the left of col
+		for (int i = 0; i < col; i++) {
+			returnNum /= 10; 
+		}
+		return returnNum;
 	}
 }
--- Tester.java
+++ Tester.java
@@ -4,5 +4,6 @@ public class Tester {
 		System.out.println(Radix.nth(-123,1));
 		System.out.println(Radix.nth(123,2));
 		System.out.println(Radix.nth(-123,2));
+		System.out.println(27/4);
 	}
 }
