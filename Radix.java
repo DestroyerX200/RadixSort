23d3be24cc4c464f699f4b34ce153891cdf5f07f
i has candys <ichen10@stuy.edu>
2021-01-14 12:49:17 -0500

    modified nth method in Radix.java to make it more efficient

--- Radix.java
+++ Radix.java
@@ -1,11 +1,10 @@
 public class Radix {
 	public static int nth(int n, int col) {
 		int k = Math.abs(n);
-		k %= Math.pow(10, col+1); //truncating digits to the left of col
 		for (int i = 0; i < col; i++) {
 			k /= 10; 
 		}
-		return k;
+		return k % 10;
 	}
 	public static int length(int n) {
 		if (n == 0) {
